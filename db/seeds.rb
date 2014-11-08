b7327fdb4606b4a961b9e8466d5c7c0e347865dd
Chris Masters <chrismasters@gmail.com>
2014-11-08 14:03:41 +0100

    Some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Famille.delete_all
+
+Famille.create([
+  { nom: 'Agavacees' },
+  { nom: 'Alsotroemeriacees' },
+  { nom: 'Amaranthacees' },
+  { nom: 'Amaryllidacees' }
+])
